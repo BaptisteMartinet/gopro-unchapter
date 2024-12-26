481dc9eea1b5ad21f9f0f26236e67a662f30453f
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-12-27 00:43:34 +0100

    feat: add simple logs

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -21,16 +21,23 @@ export default function Home() {
         const { type, encoding, chapter } = info;
         return `${type}:${encoding}:${chapter}`;
       });
+      console.log(`Concatenating ${filesWithInfo.length} files into ${indexedFiles.size} files.`);
       await ensureFFmpegLoaded();
       for (const indexedFile of indexedFiles.values()) {
+        console.log(`Writing ${indexedFile.length} files.`);
         for (const file of indexedFile)
           await ffmpeg.writeFile(file.file.name, await fetchFile(file.file));
+        console.log('Writing text content');
         const textContent = indexedFile.map(({ file }) => `file '${file.name}'`).join('\n');
         await ffmpeg.writeFile('copyContent.txt', textContent);
+        console.log('Concatenating');
         await ffmpeg.exec(['-f', 'concat', '-safe', '0', '-i', 'copyContent.txt', '-c', 'copy', 'output.mp4']);
+        console.log('Reading');
         const data = await ffmpeg.readFile("output.mp4");
         const indexInfo = indexedFile[0].info;
+        console.log('Blobing');
         saveFile(new Blob([data], { type: "video/mp4" }), `${indexInfo.chapter}.mp4`);
+        console.log('Cleaning up');
         await ffmpeg.deleteFile('output.mp4');
         await ffmpeg.deleteFile('copyContent.txt');
         for (const file of indexedFile)
