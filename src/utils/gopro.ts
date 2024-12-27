62a4018d72ed4d9705cf496e62edd1a2c9792cfa
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-12-27 23:03:38 +0100

    fix: fix gopro regex and add mimetype

--- src/utils/gopro.ts
+++ src/utils/gopro.ts
@@ -1,7 +1,7 @@
 import assert from "assert";
 import { isStrNum } from "./string";
 // Doc https://community.gopro.com/s/article/GoPro-Camera-File-Naming-Convention?language=en_US
-const GoproFilenameRegex = /^G([HX])(\d\d|[A-Z][A-Z])(\d\d\d\d).mp4|MP4$/;
+const GoproFilenameRegex = /^G([HX])(\d\d|[A-Z][A-Z])(\d\d\d\d).(mp4|MP4)$/;
 
 export function parseGoproFilename(filename: string) {
   const res = GoproFilenameRegex.exec(filename);
@@ -12,14 +12,20 @@ export function parseGoproFilename(filename: string) {
   const rawEncoding = res.at(1);
   const rawChapter = res.at(2);
   const rawFileNumber = res.at(3);
-  assert(rawEncoding && rawChapter && rawFileNumber);
+  const rawMimeType = res.at(4);
+  assert(
+    rawEncoding && rawChapter && rawFileNumber,
+    `Filename: ${filename}, Encoding: ${rawEncoding}, chapter: ${rawChapter}, fileNumber: ${rawFileNumber}, mimeType: ${rawMimeType}`
+  );
   const encoding = rawEncoding === "H" ? "AVC" : "HEVC";
   const chapter = rawChapter;
   const fileNumber = Number(rawFileNumber);
+  const mimeType = rawMimeType;
   return {
     type: isStrNum(rawChapter) ? "chaptered" : "looping",
     encoding,
     chapter,
     fileNumber,
+    mimeType,
   } as const;
 }
