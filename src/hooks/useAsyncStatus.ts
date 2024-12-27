62d1809558a36c42c55325156c0b58bea5d9d398
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-12-27 21:50:32 +0100

    feat: add promise to pending async status

--- src/hooks/useAsyncStatus.ts
+++ src/hooks/useAsyncStatus.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export type AsyncStatus<T> =
   | { status: "uninitialized" }
-  | { status: "pending" }
+  | { status: "pending"; promise: Promise<T> }
   | { status: "fulfilled"; value: T }
   | { status: "rejected"; error: Error };
 
@@ -12,7 +12,7 @@ export function useAsyncStatus<T>() {
   });
 
   const track = (promise: Promise<T>) => {
-    setStatus({ status: "pending" });
+    setStatus({ status: "pending", promise });
     promise
       .then((res) => setStatus({ status: "fulfilled", value: res }))
       .catch((err) => setStatus({ status: "rejected", error: err }));
