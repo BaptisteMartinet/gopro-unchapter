da57a5d85d412165df92d85cc8c0361b11ab635d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-12-27 23:41:59 +0100

    style: minor

--- src/utils/saveFile.ts
+++ src/utils/saveFile.ts
@@ -1,6 +1,6 @@
 export function saveFile(blob: Blob, filename: string) {
   const blobURL = URL.createObjectURL(blob);
-  const downloadLink = document.createElement('a');
+  const downloadLink = document.createElement("a");
   downloadLink.href = blobURL;
   downloadLink.download = filename;
   document.body.appendChild(downloadLink);
