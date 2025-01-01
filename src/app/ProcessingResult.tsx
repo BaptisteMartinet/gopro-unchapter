d221dd274bf3d017f8eb6c781bf18d48f3fbb2ad
Baptiste Martinet <baptiste.martinet@epitech.eu>
2025-01-01 23:00:23 +0100

    feat: setup zip filename

--- src/app/ProcessingResult.tsx
+++ src/app/ProcessingResult.tsx
@@ -1,6 +1,20 @@
 import { Button, Stack } from '@mantine/core';
 import { IconDownload } from '@tabler/icons-react';
 
+function formatDatePart(part: number) {
+  return part.toString().padStart(2, '0');
+}
+
+function makeZipFilename() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = formatDatePart(now.getMonth() + 1);
+  const day = formatDatePart(now.getDate());
+  const hours = formatDatePart(now.getHours());
+  const minutes = formatDatePart(now.getMinutes());
+  return `${year}-${month}-${day}_${hours}-${minutes}_gopro-unchapter.zip`;
+}
+
 export interface ProcessingResultProps {
   zipURL: string;
   onReset: () => void;
@@ -14,7 +28,7 @@ export default function ProcessingResult(props: ProcessingResultProps) {
       <Button
         component="a"
         href={zipURL}
-        download="zipTest.zip"
+        download={makeZipFilename()}
         size="lg"
         rightSection={<IconDownload />}
       >
